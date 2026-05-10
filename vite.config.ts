7eb369f7ea9203043c768ae4f21bbb59bed0e600
sanyue <liang452@purdue.edu>
2026-05-10 01:08:18 -0400

    path fix finally

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-// https://vite.dev/config/
 import { defineConfig } from "vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/ocean-temp-game/",
+  
+  server: {
+    port: 8080,
+    open: true,
+  },
 });
